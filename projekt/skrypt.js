359ffb6f9e780ddde33d48f5d7347dcc21994f65
Dankino <89976283+Kinowski-Daniel@users.noreply.github.com>
2023-11-23 19:42:09 +0100

    Update skrypt.js
    Dodałem skrypt do wyszukiwarki

--- projekt/skrypt.js
+++ projekt/skrypt.js
@@ -1,1 +1,50 @@
+const API_KEY = 'b2676e3dfe0b9b9d794b8fe3233110fc';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.getElementById('searchButton');
+  const searchInput = document.getElementById('searchInput');
+  const moviesContainer2 = document.getElementById('moviesContainer2');
+
+  searchButton.addEventListener('click', () => {
+    const query = searchInput.value;
+    searchMovie(query);
+  });
+
+  function searchMovie(query) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
+
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then(data => {
+        displayMovies(data.results);
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+      });
+  }
+
+  function displayMovies(movies) {
+    moviesContainer2.innerHTML = '';
+
+    movies.forEach(movie => {
+      if (movie.poster_path) {
+        const movieLink = document.createElement('a');
+        movieLink.classList.add('movie');
+        movieLink.setAttribute('target', '_blank'); // Otwieranie linku w nowej karcie
+        movieLink.href = `https://www.themoviedb.org/movie/${movie.id}`;
+
+        const moviePoster = document.createElement('img');
+        moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+        moviePoster.alt = movie.title;
+
+        movieLink.appendChild(moviePoster);
+        moviesContainer2.appendChild(movieLink);
+      }
+    });
+  }
+});
