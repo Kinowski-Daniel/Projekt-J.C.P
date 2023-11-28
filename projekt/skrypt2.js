3e10d477d7285b53c3e060c4db5f9a63c9f8dafe
Dankino <89976283+Kinowski-Daniel@users.noreply.github.com>
2023-11-28 01:18:31 +0100

    rating dodany

--- projekt/skrypt2.js
+++ projekt/skrypt2.js
@@ -26,9 +26,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const gatunki = movie.genres.map(gatunek => gatunek.name).join(', ');
     document.getElementById('genres').innerText = gatunki;
     
-
+    // Reżyser - tutaj może być konieczne inne API lub źródło danych, aby uzyskać informacje o reżyserze
+    // Dla uproszczenia, załóżmy, że nie jest dostępne bezpośrednio i pozostawiamy to puste na razie
     document.getElementById('director').innerText = 'Nie dostępne';
-  
+    
+    // Aktorzy - podobnie jak w przypadku reżysera, zakładamy, że nie są dostępni bezpośrednio
     document.getElementById('actors').innerText = 'Nie dostępne';
 
     document.getElementById('overview').innerText = movie.overview;
@@ -52,4 +54,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const trailerElement = document.getElementById('trailer');
     trailerElement.innerHTML = `<a href="${trailerLink}" target="">Zobacz zwiastun na YouTube</a>`;
   };
+  const submitRatingg = () => {
+    const rating = parseFloat(document.getElementById('ratingSlider').value);
+
+    // Sprawdź, czy ocena mieści się w zakresie 0-10
+    if (rating >= 0 && rating <= 10) {
+      // Zapisz ocenę w localStorage
+      localStorage.setItem(`movie_${movieId}_rating`, rating);
+      console.log('Ocena została zapisana lokalnie.');
+      // Możesz dodać tutaj aktualizację strony lub informację zwrotną dla użytkownika
+    } else {
+      console.error('Ocena musi być w zakresie od 0 do 10.');
+    }
+  };
+
+  document.getElementById('submitRating').addEventListener('click', submitRatingg);
+
+  // Pobierz ocenę z localStorage przy ładowaniu strony
+  const storedRating = localStorage.getItem(`movie_${movieId}_rating`);
+  if (storedRating) {
+    document.getElementById('ratingSlider').value = storedRating;
+  }
 });
+
