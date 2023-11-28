8f9f836c797adf16e2c60576f52fdde10fd64a40
Dankino <89976283+Kinowski-Daniel@users.noreply.github.com>
2023-11-28 17:28:09 +0100

    Poprawiony wygląd kodu

--- projekt/skrypt.js
+++ projekt/skrypt.js
@@ -2,141 +2,139 @@ const API_KEY = 'b2676e3dfe0b9b9d794b8fe3233110fc';
 const MAX_MOVIES_TO_DISPLAY = 5;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const searchButton = document.getElementById('searchButton');
-  const searchInput = document.getElementById('searchInput');
-  const genreSelect = document.getElementById('genreSelect'); // Dodany select z gatunkami
-  const moviesContainer2 = document.getElementById('moviesContainer2');
+	const searchButton = document.getElementById('searchButton');
+	const searchInput = document.getElementById('searchInput');
+	const genreSelect = document.getElementById('genreSelect');
+	const moviesContainer2 = document.getElementById('moviesContainer2');
 
-  searchButton.addEventListener('click', () => {
-    const query = searchInput.value;
-    const selectedGenre = genreSelect.value; // Pobieranie wybranego gatunku
-    searchMovie(query, selectedGenre);
-  });
-  
+	searchButton.addEventListener('click', () => {
+		const query = searchInput.value;
+		const selectedGenre = genreSelect.value; //Pobiera gatunek 
+		searchMovie(query, selectedGenre);
+	});
 
-  const searchMovie = (query, genre) => {
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
 
-    if (genre) {
-      url += `&with_genres=${genre}`; // Dodanie gatunku do URL-a zapytania
-    }
+	const searchMovie = (query, genre) => {
+		let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`;
 
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error('Network response was not ok.');
-      })
-      .then(data => {
-        displayMovies(data.results);
-      })
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation:', error);
-      });
-  };
+		if (genre) {
+			url += `&with_genres=${genre}`; 
+		}
 
-  const displayMovies = (movies) => {
-    moviesContainer2.innerHTML = '';
+		fetch(url)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Network response was not ok.');
+			})
+			.then(data => {
+				displayMovies(data.results);
+			})
+			.catch(error => {
+				console.error('There has been a problem with your fetch operation:', error);
+			});
+	};
 
-    movies.forEach(movie => {
-      if (movie.poster_path) {
-        const movieLink = document.createElement('a');
-        movieLink.href = `index2.html?id=${movie.id}`; // Dodanie identyfikatora filmu do adresu URL
-        movieLink.setAttribute('target', '');
-        movieLink.classList.add('movie');
+	const displayMovies = (movies) => {
+		moviesContainer2.innerHTML = '';
 
-        const moviePoster = document.createElement('img');
-        moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        moviePoster.alt = movie.title;
+		movies.forEach(movie => {
+			if (movie.poster_path) {
+				const movieLink = document.createElement('a');
+				movieLink.href = `index2.html?id=${movie.id}`;
+				movieLink.setAttribute('target', '');
+				movieLink.classList.add('movie');
 
-        movieLink.appendChild(moviePoster);
-        moviesContainer2.appendChild(movieLink);
-      }
-    });
-  };
+				const moviePoster = document.createElement('img');
+				moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+				moviePoster.alt = movie.title;
+
+				movieLink.appendChild(moviePoster);
+				moviesContainer2.appendChild(movieLink);
+			}
+		});
+	};
 });
 
-  // Kod dla popularnych filmów
+// Kod dla popularnych filmów
 
 document.addEventListener('DOMContentLoaded', () => {
-  const moviesContainer = document.getElementById('moviesContainer');
+	const moviesContainer = document.getElementById('moviesContainer');
 
-  fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error('Network response was not ok.');
-    })
-    .then(data => {
-      displayPopularMovies(data.results);
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
+	fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error('Network response was not ok.');
+		})
+		.then(data => {
+			displayPopularMovies(data.results);
+		})
+		.catch(error => {
+			console.error('There has been a problem with your fetch operation:', error);
+		});
 
-  const displayPopularMovies = (movies) => {
-    movies.slice(0, MAX_MOVIES_TO_DISPLAY).forEach(movie => {
-      if (movie.poster_path) {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie-poster');
+	const displayPopularMovies = (movies) => {
+		movies.slice(0, MAX_MOVIES_TO_DISPLAY).forEach(movie => {
+			if (movie.poster_path) {
+				const movieElement = document.createElement('div');
+				movieElement.classList.add('movie-poster');
 
-        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        const title = movie.title;
-        const releaseDate = movie.release_date;
+				const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+				const title = movie.title;
+				const releaseDate = movie.release_date;
 
-        movieElement.innerHTML = `
+				movieElement.innerHTML = `
           <a href="index2.html?id=${movie.id}" class="movie-link">
             <img src="${imageUrl}" alt="${title}" data-movie-id="${movie.id}">
           </a>
           
         `;
 
-        moviesContainer.appendChild(movieElement);
-      }
-    });
-  };
+				moviesContainer.appendChild(movieElement);
+			}
+		});
+	};
 });
 
-//----------------------------------------------------------------------------------
-const sortSelect = document.getElementById('sortSelect'); // Dodane rozwijane menu z opcjami sortowania
-  sortSelect.addEventListener('change', () => {
-    const selectedSortOption = sortSelect.value;
-    if (selectedSortOption === 'release_date') {
-      // Sortowanie filmów po roku wydania
-      const movies = Array.from(moviesContainer2.querySelectorAll('.movie'));
-      movies.sort((a, b) => {
-        const movieAYear = parseInt(a.dataset.releaseYear);
-        const movieBYear = parseInt(b.dataset.releaseYear);
-        return movieBYear - movieAYear;
-      });
-      // Ponowne umieszczenie posortowanej listy filmów w kontenerze
-      moviesContainer2.innerHTML = '';
-      movies.forEach(movie => {
-        moviesContainer2.appendChild(movie);
-      });
-    }
-  });
+// Kod Do Sortowania
+const sortSelect = document.getElementById('sortSelect'); //Rozwijane menu z opcjami sortowania
+sortSelect.addEventListener('change', () => {
+	const selectedSortOption = sortSelect.value;
+	if (selectedSortOption === 'release_date') {
+		// Sortowanie filmów po roku wydania
+		const movies = Array.from(moviesContainer2.querySelectorAll('.movie'));
+		movies.sort((a, b) => {
+			const movieAYear = parseInt(a.dataset.releaseYear);
+			const movieBYear = parseInt(b.dataset.releaseYear);
+			return movieBYear - movieAYear;
+		});
+		moviesContainer2.innerHTML = '';
+		movies.forEach(movie => {
+			moviesContainer2.appendChild(movie);
+		});
+	}
+});
 
-  const displayMovies = (movies) => {
-    moviesContainer2.innerHTML = 'moviesContainer2';
+const displayMovies = (movies) => {
+	moviesContainer2.innerHTML = 'moviesContainer2';
 
-    movies.forEach(movie => {
-      if (movie.poster_path) {
-        const movieLink = document.createElement('a');
-        movieLink.href = `index.html?id=${movie.id}`;
-        movieLink.setAttribute('target', '');
-        movieLink.classList.add('movie');
-        movieLink.dataset.releaseYear = movie.release_date.slice(0, 4); // Dodanie roku wydania do dataset
+	movies.forEach(movie => {
+		if (movie.poster_path) {
+			const movieLink = document.createElement('a');
+			movieLink.href = `index.html?id=${movie.id}`;
+			movieLink.setAttribute('target', '');
+			movieLink.classList.add('movie');
+			movieLink.dataset.releaseYear = movie.release_date.slice(0, 4); // Dodanie roku wydania do dataset
 
-        const moviePoster = document.createElement('img');
-        moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-        moviePoster.alt = movie.title;
+			const moviePoster = document.createElement('img');
+			moviePoster.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+			moviePoster.alt = movie.title;
 
-        movieLink.appendChild(moviePoster);
-        moviesContainer2.appendChild(movieLink);
-      }
-    });
-  };
-
+			movieLink.appendChild(moviePoster);
+			moviesContainer2.appendChild(movieLink);
+		}
+	});
+};
